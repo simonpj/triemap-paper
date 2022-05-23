24d84c54e8c722294d407b302c1167c2228cd4c8
Sebastian Graf <sebastian.graf@kit.edu>
2022-05-23 15:54:04 +0200

    Bump pinned nix commit

--- code/shell.nix
+++ code/shell.nix
@@ -3,9 +3,8 @@
       # Descriptive name to make the store path easier to identify
       name = "nixpkgs-unstable-2021-07-06";
       # Commit hash for nixos-unstable as of 2018-09-12
-      url = "https://github.com/nixos/nixpkgs/archive/8eb54b8e0907ce37ba0eeeacfb67676d42d857bc.tar.gz";
+      url = "https://github.com/nixos/nixpkgs/archive/dfd82985c273aac6eced03625f454b334daae2e8.tar.gz";
       # Hash obtained using `nix-prefetch-url --unpack <url>`
-      sha256 = "09pzj1122ha9rbb04m395a1wv35hjz02dq5lxs3kglndjmapc2nv";
-    }) {}
-, compiler ? "ghc8104" }:
-(nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./derivation.nix { }).env
+      sha256 = "1ipd1k1gvxh9sbg4w4cpa3585q09gvsq8xbjvxnnmfjib6r6xx4i";
+    }) {} }:
+(nixpkgs.pkgs.haskellPackages.callPackage ./derivation.nix { }).env
